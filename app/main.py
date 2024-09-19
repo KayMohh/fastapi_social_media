a98e2faac12ef8c9095ec1c326b18e42bb275ea1
kaymohh <thrilltim@gmail.com>
2024-09-19 09:01:00 +0100

    added psycopg

--- app/main.py
+++ app/main.py
@@ -87,6 +87,23 @@ def get_post(id: int, response : Response):
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
     index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"post with id : {id} does not exist")
     my_posts.pop(index)
-    return Response {status_code = status.HTTP_204_NO_CONTENT}
+    return Response(status_code = status.HTTP_204_NO_CONTENT)
     # return {"message" : "post deleted successfully"}
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"post with id : {id} does not exist")
+    post_dict = post.dict()
+    post_dict["id"] = id
+    my_posts[index] = post_dict
+    return {"data" : post_dict}
+
+    print(post)
+
