be5fb674697a6847fe165de704e69a8b9f63c9ac
kaymohh <thrilltim@gmail.com>
2024-10-15 07:26:48 +0100

    added users auth

--- app/main.py
+++ app/main.py
@@ -3,16 +3,16 @@ from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional, List
 from random import randrange
-from passlib.context import CryptContext
+
 import psycopg2
 from  psycopg2.extras import RealDictCursor 
 import time
-from . import models, schemas
+from . import models, schemas, utils
 from .database import engine, get_db
 from sqlalchemy.orm import Session
 
 
-pwd_context = CryptContect(schemes=['bcrypt'], deprecated= "auto")
+
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -127,8 +127,24 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
 
 @app.post('/users', status_code=status.HTTP_201_CREATED, response_model= schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    
+    
+    # hashing password
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+
+    
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@app.get("/users/{id}")
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"User with id : {id} does not exist")
+
+    return user
